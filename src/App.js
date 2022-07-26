bb177f0befd8c58e382194145fedf2565966a63f
Miguel Castillo <castilma95@gmail.com>
2022-07-25 18:04:54 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [errors, setErrors] = useState({
+    "name": '',
+    "email": ''
+  })
+
+  const handleChange = e =>{
+    if(e.target.id == "name"){
+      if(isValid(e.target.value, 3) == false){
+        setErrors({...errors, [e.target.id] : "Name must be greater than 3"})
+
+      } else {
+        setErrors({...errors, [e.target.id] : ""})
+      }
+    }
+    if(e.target.id == "email"){
+      if(isValid(e.target.value, 6) == false){
+        setErrors({...errors, [e.target.id] : "Email must be greater than 6"})
+      } else {
+        setErrors({...errors, [e.target.id] : ""})
+      }
+    }
+    
+  }
+
+  const isValid = (val, len) => {
+    if(val.length < len){
+      return false
+    }
+    return true
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <form>
+        <label htmlFor="name">
+          Name:
+        </label>
+        <input id="name" type="text" onChange={handleChange}></input>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {errors.name.length > 0 ? errors.name : '' }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <label htmlFor="email">
+          Email:
+        </label>
+        <input id="email" type="email" onChange={handleChange}></input>
+        <p>
+          {errors.email.length > 0 ? errors.email : '' }
+        </p>
+        <input type="submit" value="submit"/>
+      </form>
+    </>
   );
 }
 
